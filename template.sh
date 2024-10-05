a97d99c025d5a998d39ca43e09c483fb01bbb967
A1exander-liU <alexanderliu2016@gmail.com>
2024-10-04 18:35:46 -0700

    feat: update template scripts

--- template.sh
+++ template.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+pythonContents="
+class Solution:
+  def function(self) -> None:
+    return
+"
+readmeContents="
+# Problem
+
+
+### Example 1
+
+
+### Solution
+"
+
 arg1=$1
 mkdir $arg1
 cd $arg1
-touch README.md
-touch solution.py
+echo $readmeContents >README.md
+echo $pythonContents >solution.py
--- template2.sh
+++ template2.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+pythonContents="class Solution:\n\tdef function(self) -> None:\n\t\treturn"
+readmeContents="# Problem\n\n### Example 1\n\n### Solution"
+
 # Iterate over each directory in the current directory
 for dir in */; do
-	# Trim the trailing slash from the directory name
-	dir=${dir%/}
+  # Trim the trailing slash from the directory name
+  dir=${dir%/}
 
-	# Check if it's a directory
-	if [ -d "$dir" ]; then
-		# Create solution.py in the directory
-		touch "$dir/solution.py"
-    touch "$dir/README.md"
-		echo "Created solution.py in $dir"
-	fi
+  # Check if it's a directory
+  if [ -d "$dir" ]; then
+    # Create solution.py in the directory
+    echo pythonContents >"$dir/solution.py"
+    echo readmeContents >"$dir/README.md"
+    echo "Created solution.py in $dir"
+  fi
 done
